1a22c8daedce8047c798303aa8cfe246d3cd892f
pvin <praaveenranganathan@gmail.com>
2013-10-27 00:36:01 +0530

    github controller added and sample git-api code added

--- app/controllers/github_controller.rb
+++ app/controllers/github_controller.rb
@@ -1,6 +1,10 @@
 class GithubController < ApplicationController
 
-
+  def index
+    github = Github.new :client_id => "", :client_secret => ""
+    puts '**************************8'
+    puts github.repos.commits.all 'fdv', 'publify'
+    puts '*******************************'
+  end
 
 end
-
