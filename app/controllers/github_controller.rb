6850a383cc6c607de006124862c5da2f51211540
pvin <praaveenranganathan@gmail.com>
2014-01-01 17:32:20 +0530

    stable state of github

--- app/controllers/github_controller.rb
+++ app/controllers/github_controller.rb
@@ -1,10 +1,20 @@
 class GithubController < ApplicationController
 
-  def consumer
-    github = Github.new :client_id => "", :client_secret => ""
-    puts '**************************8'
-    puts github.repos.commits.all 'fdv', 'publify'
-    puts '*******************************'
+  def cosumers
+
+  end
+
+  def consumer1
+    #github = Github.new :client_id => "4c21444eb7ecee26f806", :client_secret => "0bddb2dc36faba30a2ffc93241358ebcdc7682cf"
+    #puts '**************************8'
+    #puts github.repos.commits.all 'fdv', 'publify'
+    #puts '*******************************'
+
+    @firstname = request["userName"]
+    @hash_values=Github.repos.list user: "#{@firstname}"
+    puts '============================'
+    puts     @hash_values
+    puts '============================'
   end
 
 end
