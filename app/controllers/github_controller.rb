1ed4bb05f4dc058196e6193f993b80a6c1b27bd8
pvin <praaveenranganathan@gmail.com>
2014-01-18 17:23:13 +0530

    minor refactor

--- app/controllers/github_controller.rb
+++ app/controllers/github_controller.rb
@@ -5,7 +5,7 @@ class GithubController < ApplicationController
   end
 
   def consumer1
-    #github = Github.new :client_id => "4c21444eb7ecee26f806", :client_secret => "0bddb2dc36faba30a2ffc93241358ebcdc7682cf"
+    #github = Github.new :client_id => "******", :client_secret => "************"
     #puts '**************************8'
     #puts github.repos.commits.all 'fdv', 'publify'
     #puts '*******************************'
