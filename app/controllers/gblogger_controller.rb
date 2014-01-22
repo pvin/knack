9cff02ab3b47042ccb98f3eeb4a7fce706f5d715
pvin <praaveenranganathan@gmail.com>
2014-01-22 08:40:45 +0530

    minor refactor

--- app/controllers/gblogger_controller.rb
+++ app/controllers/gblogger_controller.rb
@@ -3,7 +3,7 @@ require 'net/http'
 class GbloggerController < ApplicationController
 
   def consumer
-    response = HTTParty.get('https://www.googleapis.com/blogger/v3/blogs/3891960319537892971/posts?key=AIzaSyCmyA7TS_PMW-E42L4Fg75Lz5RZpaSpA5A')
+    response = HTTParty.get('https://www.googleapis.com/blogger/v3/blogs/3891960319537892971/posts?key=***')
     puts '-----------------'
     puts response.body
     puts '-----------------'
