aacb025d16d359f2a88771704a933fbe14433ab1
pvin <praaveenranganathan@gmail.com>
2014-01-26 19:23:58 +0530

    code added for cache fails

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,10 +1,19 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery
 
+  before_filter :set_no_cache
+
+
+
   def after_sign_in_path_for(resource)
-
     sign_in_url = "http://localhost:3000/loginhome/getoption"
-
   end
+
+  def set_no_cache
+    response.headers["Cache-Control"] = "no-cache, no-store, max-age=0, must-revalidate"
+    response.headers["Pragma"] = "no-cache"
+    response.headers["Expires"] = "Fri, 01 Jan 1990 00:00:00 GMT"
+  end
+
 
 end
