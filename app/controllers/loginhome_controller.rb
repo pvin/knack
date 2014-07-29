f09b6da7f8802549d4f88fe39fa4eabda8974ac4
pvin <praaveenranganathan@gmail.com>
2014-07-30 02:22:37 +0530

    worked on sof action

--- app/controllers/loginhome_controller.rb
+++ app/controllers/loginhome_controller.rb
@@ -1,44 +1,125 @@
+#require 'open-uri'
 class LoginhomeController < ApplicationController
 
   include HTTParty
-  base_uri "http://api.stackoverflow.com/1.1"
-
+  #base_uri "http://api.stackexchange.com/2.2"
   def getoption
 
   end
 
   def sof_consumer
-    @name = request["name"]
-    @response = self.class.get("/users/#{@name}")
-    test = @response["users"][0]["display_name"]
+    @user_id = request["user_id"]
+
+    #/users/{ids}
+    @user_info = self.class.get("http://api.stackexchange.com/2.2/users/#{@user_id}?order=desc&sort=reputation&site=stackoverflow")
+
+    #/users/{ids}/answers
+    @user_answer = self.class.get("http://api.stackexchange.com/2.2/users/#{@user_id}/answers?order=desc&sort=activity&site=stackoverflow")
+    @user_answer_count = @user_answer["items"].count
+    @answer_collect = Array.new()
+    @user_answer["items"].each { |item| @answer_collect << "http://stackoverflow.com/q/#{item["answer_id"]}"}
+
+    #/users/{ids}/questions
+    @user_question = self.class.get("http://api.stackexchange.com/2.2/users/#{@user_id}/questions?order=desc&sort=activity&site=stackoverflow")
+    @user_question_count = @user_question["items"].count
+    @question_collect = Array.new()
+    @user_question["items"].each { |item| @question_collect << "http://stackoverflow.com/q/#{item["question_id"]}"}
+
+    #/users/{id}/network-activity
+    @user_network_activity = self.class.get("http://api.stackexchange.com/2.2/users/#{@user_info["items"][0]["account_id"]}/network-activity")
+    @user_network_activity_count = @user_network_activity["items"].count
+
+    #/users/{ids}/reputation-history
+    @user_reputation_history = self.class.get("http://api.stackexchange.com/2.2/users/#{@user_id}/reputation-history?site=stackoverflow")
+    @user_reputation_array = Array.new()
+    @user_reputation_history["items"].each { |repu| @user_reputation_array << repu["reputation_change"] }
+
+    #/users/{ids}/tags
+    @user_tags = self.class.get("http://api.stackexchange.com/2.2/users/#{@user_id}/tags?order=desc&sort=popular&site=stackoverflow")
+    @user_tags_info_hash = Hash.new()
+    @user_tags["items"].each { |tag| @user_tags_info_hash["#{tag["name"]}"] = tag["count"]}
+
+    #/users/{ids}/associated
+    @user_association = self.class.get("http://api.stackexchange.com/2.2/users/#{@user_info["items"][0]["account_id"]}/associated")
+    @user_association_hash = Hash.new()
+    @user_association["items"].each { |asso| @user_association_hash["#{asso["site_name"]}"] = asso["reputation"] }
+
+    #/users/{ids}/timeline
+    @user_timeline = self.class.get("http://api.stackexchange.com/2.2/users/#{@user_id}/timeline?site=stackoverflow")
+    @user_timeline_count = @user_timeline["items"].count
+
+    puts '+++'
+              puts @user_association
+    puts '+++'
+
     respond_to do |format|
       format.html
       format.pdf do
         pdf = Prawn::Document.new
-        logopath = "public/pie-chart-1.png"
-        pdf.image logopath, :width => 197, :height => 120
-
+        @logopath = "public/pie-chart-1.png"
+        @user_image = "#{@user_info["items"][0]["profile_image"]}"
+        pdf.image @logopath, :width => 197, :height => 120
+        #pdf.image open (@user_image)
         pdf.fill_color "0066FF"
         pdf.font_size 42
         pdf.text_box "Knack Reports", :align => :right
 
         pdf.move_down 20
         pdf.font_size 14
-        pdf.text "Below generated report for stack overflow user #{@response["users"][0]["display_name"]}"
+        pdf.text "Below generated report for stack overflow user #{@user_info["items"][0]["display_name"]}"
 
         pdf.move_down 20
-        to_show = [["UserId", "#{@response["users"][0]["user_id"]}"],
-                   ["Desplay Name", "#{@response["users"][0]["display_name"]}"],
-                   ["Reputation", "#{@response["users"][0]["reputation"]}"],
-                   ["Location", "#{@response["users"][0]["location"]}"],
-                   ["Question Count", "#{@response["users"][0]["question_count"]}"],
-                   ["Answer Count", "#{@response["users"][0]["answer_count"]}"],
-                   ["View Count", "#{@response["users"][0]["view_count"]}"],
-                   ["Up Vote Count", "#{@response["users"][0]["up_vote_count"]}"],
-                   ["Down Vote Count", "#{@response["users"][0]["down_vote_count"]}"],
-                   ["User Gold Badge", "#{@response["users"][0]["badge_counts"]["gold"]}"],
-                   ["User Silver Badge", "#{@response["users"][0]["badge_counts"]["silver"]}"],
-                   ["User Bronze Badge", "#{@response["users"][0]["badge_counts"]["bronze"]}"]
+        to_show = [
+                   #/users/{ids}
+
+                   ["User Id", "#{@user_info["items"][0]["user_id"]}"],
+                   ["Display Name", "#{@user_info["items"][0]["display_name"]}"],
+                   ["Location", "#{@user_info["items"][0]["location"]}"],
+                   ["Last modified Date", "#{@user_info["items"][0]["last_modified_date"]}"],
+                   ["Last Access Date", "#{@user_info["items"][0]["last_access_date"]}"],
+                   ["Reputation Change In a Current Year", "#{@user_info["items"][0]["reputation_change_year"]}"],
+                   ["Reputation Change In a Current Quarter", "#{@user_info["items"][0]["reputation_change_quarter"]}"],
+                   ["Reputation Change In a Current Month", "#{@user_info["items"][0]["reputation_change_month"]}"],
+                   ["Reputation Change In a Current Week", "#{@user_info["items"][0]["reputation_change_week"]}"],
+                   ["Reputation Change In a Current Day", "#{@user_info["items"][0]["reputation_change_day"]}"],
+                   ["Over All Reputation", "#{@user_info["items"][0]["reputation"]}"],
+                   ["Personal Website Url", "#{@user_info["items"][0]["website_url"]}"],
+                   ["SOF Link", "#{@user_info["items"][0]["link"]}"],
+                   ["Image Link", "#{@user_info["items"][0]["profile_image"]}"],
+                   ["User Gold Badge", "#{@user_info["items"][0]["badge_counts"]["gold"]}"],
+                   ["User Silver Badge", "#{@user_info["items"][0]["badge_counts"]["silver"]}"],
+                   ["User Bronze Badge", "#{@user_info["items"][0]["badge_counts"]["bronze"]}"],
+
+                   #/users/{ids}/answers
+
+                   ["User Answer Count", "#{@user_answer_count}"],    
+                   ["Link to the answers", "#{@answer_collect}"],
+
+                   #/users/{ids}/questions
+
+                   ["User Question Count", "#{@user_question_count}"],
+                   ["Link to the questions", "#{@question_collect}"],
+
+                   #/users/{id}/network-activity
+                   # have details about edit, post etc.. except accepted answer
+                   ["User Network Activity Count(Stack Exchange network, ex:stack overflow,Ask Ubuntu etc..)", "#{@user_network_activity_count}"],
+
+                   #/users/{ids}/reputation-history
+
+                   ["User Reputation History", "#{@user_reputation_array}"],
+
+                   #/users/{ids}/tags
+
+                   ["User Tags and Discussion Count", "#{@user_tags_info_hash}"],
+
+                   #/users/{ids}/associated
+
+                   ["User Association and Reputation", "#{@user_association_hash}"],
+
+                   #/users/{ids}/timeline
+
+                   ["User Actions in Stackoverflow(answered, commented, revision, accepted  etc..)", "#{@user_timeline_count}"]
+
         ]
         pdf.table(to_show) do |table|
           table.rows(1..2).width = 270
