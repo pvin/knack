2031f7dfec3aa97f0eb5714abdfdf82aef74210f
pvin <praaveenranganathan@gmail.com>
2014-10-23 00:46:09 +0530

    for production env

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -63,6 +63,7 @@ Knack::Application.configure do
   
 
   Rails.application.routes.default_url_options[:host] = 'sleepy-peak-6931.herokuapp.com'
+  #Rails.application.routes.default_url_options[:host] = 'localhost:3000'
   # Log the query plan for queries taking more than this (works
   # with SQLite, MySQL, and PostgreSQL)
   # config.active_record.auto_explain_threshold_in_seconds = 0.5
