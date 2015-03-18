f5667b6ca4e71e9169f7a637eab25a8d0819a7e7
pvin <praaveenranganathan@gmail.com>
2015-03-18 19:00:18 +0530

    maths module

--- app/controllers/mathamatician_controller.rb
+++ app/controllers/mathamatician_controller.rb
@@ -1,2 +1,25 @@
+require './lib/pdf_generator/pdf_generator.rb'
+require './lib/content_generator/content_generator'
+
 class MathamaticianController < ApplicationController
+  include HTTParty
+  include PdfGenerator
+  include ContentGenerator
+  before_filter :authenticate_user!
+
+  rescue_from CustomExceptions::HandleIfError, with: :getoption
+  def getoption
+    render 'getoption.html.erb'
+  end
+
+  def maths_consumer
+    begin
+      maths_content_processor
+      maths_pdf_responder
+    rescue Exception =>e
+      puts 'Exception at Mathematics starts * ' + e.message + '*' + e.backtrace.join("\n").to_s + ' for Mathematics id ' + @user_id + ' * end up here'
+      flash.now[:error] = "data not found."
+      raise CustomExceptions::HandleIfError
+    end
+  end
 end
