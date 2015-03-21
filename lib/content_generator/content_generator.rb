97520960f54259082c5461b2b4275c85f18d4750
pvin <praaveenranganathan@gmail.com>
2015-03-21 13:32:14 +0530

    implementation of superuser, askubuntu, server fault

--- lib/content_generator/content_generator.rb
+++ lib/content_generator/content_generator.rb
@@ -409,4 +409,194 @@ module ContentGenerator
     @user_timeline_count = @user_timeline["items"].count
 
   end
+
+  def server_f_content_processor
+    @user_id = request["sfault_user_id"]
+
+    #/users/{ids}
+    @user_info = self.class.get("http://api.stackexchange.com/2.2/users/#{@user_id}?order=desc&sort=reputation&site=serverfault")
+
+    #/users/{ids}/answers
+    @user_answer = self.class.get("http://api.stackexchange.com/2.2/users/#{@user_id}/answers?order=desc&sort=activity&site=serverfault")
+    @user_answer_count = @user_answer["items"].count
+    @answer_collect = Array.new()
+    if @user_answer["items"] != nil
+      @user_answer["items"].each { |item| @answer_collect << "http://stackoverflow.com/q/#{item["answer_id"]}" }
+    else
+      @user_answer["items"] = nil
+    end
+
+    #/users/{ids}/questions
+    @user_question = self.class.get("http://api.stackexchange.com/2.2/users/#{@user_id}/questions?order=desc&sort=activity&site=serverfault")
+    @user_question_count = @user_question["items"].count
+    @question_collect = Array.new()
+    if @user_question["items"] != nil
+      @user_question["items"].each { |item| @question_collect << "http://stackoverflow.com/q/#{item["question_id"]}" }
+    else
+      @user_question["items"] = nil
+    end
+
+    #/users/{id}/network-activity
+    @user_network_activity = self.class.get("http://api.stackexchange.com/2.2/users/#{@user_info["items"][0]["account_id"]}/network-activity")
+    @user_network_activity_count = @user_network_activity["items"].count
+
+    #/users/{ids}/reputation-history
+    @user_reputation_history = self.class.get("http://api.stackexchange.com/2.2/users/#{@user_id}/reputation-history?site=serverfault")
+    @user_reputation_array = Array.new()
+    if @user_reputation_history["items"] != nil
+      @user_reputation_history["items"].each { |repu| @user_reputation_array << repu["reputation_change"] }
+    else
+      @user_reputation_history["items"] = nil
+    end
+
+    #/users/{ids}/tags
+    @user_tags = self.class.get("http://api.stackexchange.com/2.2/users/#{@user_id}/tags?order=desc&sort=popular&site=serverfault")
+    @user_tags_info_hash = Hash.new()
+    if @user_tags["items"] != nil
+      @user_tags["items"].each { |tag| @user_tags_info_hash["#{tag["name"]}"] = tag["count"] }
+    else
+      @user_tags["items"] = nil
+    end
+
+    #/users/{ids}/associated
+    @user_association = self.class.get("http://api.stackexchange.com/2.2/users/#{@user_info["items"][0]["account_id"]}/associated")
+    @user_association_hash = Hash.new()
+    if @user_association["items"] != nil
+      @user_association["items"].each { |asso| @user_association_hash["#{asso["site_name"]}"] = asso["reputation"] }
+    else
+      @user_association["items"] = nil
+    end
+
+    #/users/{ids}/timeline
+    @user_timeline = self.class.get("http://api.stackexchange.com/2.2/users/#{@user_id}/timeline?site=serverfault")
+    @user_timeline_count = @user_timeline["items"].count
+
+  end
+
+  def super_u_content_processor
+    @user_id = request["suser_user_id"]
+
+    #/users/{ids}
+    @user_info = self.class.get("http://api.stackexchange.com/2.2/users/#{@user_id}?order=desc&sort=reputation&site=superuser")
+
+    #/users/{ids}/answers
+    @user_answer = self.class.get("http://api.stackexchange.com/2.2/users/#{@user_id}/answers?order=desc&sort=activity&site=superuser")
+    @user_answer_count = @user_answer["items"].count
+    @answer_collect = Array.new()
+    if @user_answer["items"] != nil
+      @user_answer["items"].each { |item| @answer_collect << "http://stackoverflow.com/q/#{item["answer_id"]}" }
+    else
+      @user_answer["items"] = nil
+    end
+
+    #/users/{ids}/questions
+    @user_question = self.class.get("http://api.stackexchange.com/2.2/users/#{@user_id}/questions?order=desc&sort=activity&site=superuser")
+    @user_question_count = @user_question["items"].count
+    @question_collect = Array.new()
+    if @user_question["items"] != nil
+      @user_question["items"].each { |item| @question_collect << "http://stackoverflow.com/q/#{item["question_id"]}" }
+    else
+      @user_question["items"] = nil
+    end
+
+    #/users/{id}/network-activity
+    @user_network_activity = self.class.get("http://api.stackexchange.com/2.2/users/#{@user_info["items"][0]["account_id"]}/network-activity")
+    @user_network_activity_count = @user_network_activity["items"].count
+
+    #/users/{ids}/reputation-history
+    @user_reputation_history = self.class.get("http://api.stackexchange.com/2.2/users/#{@user_id}/reputation-history?site=superuser")
+    @user_reputation_array = Array.new()
+    if @user_reputation_history["items"] != nil
+      @user_reputation_history["items"].each { |repu| @user_reputation_array << repu["reputation_change"] }
+    else
+      @user_reputation_history["items"] = nil
+    end
+
+    #/users/{ids}/tags
+    @user_tags = self.class.get("http://api.stackexchange.com/2.2/users/#{@user_id}/tags?order=desc&sort=popular&site=superuser")
+    @user_tags_info_hash = Hash.new()
+    if @user_tags["items"] != nil
+      @user_tags["items"].each { |tag| @user_tags_info_hash["#{tag["name"]}"] = tag["count"] }
+    else
+      @user_tags["items"] = nil
+    end
+
+    #/users/{ids}/associated
+    @user_association = self.class.get("http://api.stackexchange.com/2.2/users/#{@user_info["items"][0]["account_id"]}/associated")
+    @user_association_hash = Hash.new()
+    if @user_association["items"] != nil
+      @user_association["items"].each { |asso| @user_association_hash["#{asso["site_name"]}"] = asso["reputation"] }
+    else
+      @user_association["items"] = nil
+    end
+
+    #/users/{ids}/timeline
+    @user_timeline = self.class.get("http://api.stackexchange.com/2.2/users/#{@user_id}/timeline?site=superuser")
+    @user_timeline_count = @user_timeline["items"].count
+
+  end
+
+  def ask_u_content_processor
+    @user_id = request["aubuntu_user_id"]
+
+    #/users/{ids}
+    @user_info = self.class.get("http://api.stackexchange.com/2.2/users/#{@user_id}?order=desc&sort=reputation&site=askubuntu")
+
+    #/users/{ids}/answers
+    @user_answer = self.class.get("http://api.stackexchange.com/2.2/users/#{@user_id}/answers?order=desc&sort=activity&site=askubuntu")
+    @user_answer_count = @user_answer["items"].count
+    @answer_collect = Array.new()
+    if @user_answer["items"] != nil
+      @user_answer["items"].each { |item| @answer_collect << "http://stackoverflow.com/q/#{item["answer_id"]}" }
+    else
+      @user_answer["items"] = nil
+    end
+
+    #/users/{ids}/questions
+    @user_question = self.class.get("http://api.stackexchange.com/2.2/users/#{@user_id}/questions?order=desc&sort=activity&site=askubuntu")
+    @user_question_count = @user_question["items"].count
+    @question_collect = Array.new()
+    if @user_question["items"] != nil
+      @user_question["items"].each { |item| @question_collect << "http://stackoverflow.com/q/#{item["question_id"]}" }
+    else
+      @user_question["items"] = nil
+    end
+
+    #/users/{id}/network-activity
+    @user_network_activity = self.class.get("http://api.stackexchange.com/2.2/users/#{@user_info["items"][0]["account_id"]}/network-activity")
+    @user_network_activity_count = @user_network_activity["items"].count
+
+    #/users/{ids}/reputation-history
+    @user_reputation_history = self.class.get("http://api.stackexchange.com/2.2/users/#{@user_id}/reputation-history?site=askubuntu")
+    @user_reputation_array = Array.new()
+    if @user_reputation_history["items"] != nil
+      @user_reputation_history["items"].each { |repu| @user_reputation_array << repu["reputation_change"] }
+    else
+      @user_reputation_history["items"] = nil
+    end
+
+    #/users/{ids}/tags
+    @user_tags = self.class.get("http://api.stackexchange.com/2.2/users/#{@user_id}/tags?order=desc&sort=popular&site=askubuntu")
+    @user_tags_info_hash = Hash.new()
+    if @user_tags["items"] != nil
+      @user_tags["items"].each { |tag| @user_tags_info_hash["#{tag["name"]}"] = tag["count"] }
+    else
+      @user_tags["items"] = nil
+    end
+
+    #/users/{ids}/associated
+    @user_association = self.class.get("http://api.stackexchange.com/2.2/users/#{@user_info["items"][0]["account_id"]}/associated")
+    @user_association_hash = Hash.new()
+    if @user_association["items"] != nil
+      @user_association["items"].each { |asso| @user_association_hash["#{asso["site_name"]}"] = asso["reputation"] }
+    else
+      @user_association["items"] = nil
+    end
+
+    #/users/{ids}/timeline
+    @user_timeline = self.class.get("http://api.stackexchange.com/2.2/users/#{@user_id}/timeline?site=askubuntu")
+    @user_timeline_count = @user_timeline["items"].count
+
+  end
+
 end
